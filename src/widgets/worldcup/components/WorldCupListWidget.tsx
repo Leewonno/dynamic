bc2c83c56ded89c62793e27924f285eff54e5ca2
Leewonno <dldnjssh123@naver.com>
2025-10-09 01:04:45 +0900

    feat : worldcup

---
+++ src/widgets/worldcup/components/WorldCupListWidget.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+const Widget = styled.div`
+  
+`
+
+const temp = [
+  {
+    title:'최신 여자아이돌 이상형월드컵',
+    cover:'https://newsimg.sedaily.com/2024/07/18/2DBSEBEB1N_1.jpg',
+  },
+  {
+    title:'최신 남자아이돌 이상형월드컵',
+    cover:'https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2025/01/17/4777a40d-4cd1-4828-a0c1-a1d5bd7bdbbd.jpg',
+  },
+  {
+    title:'최신 아이돌음악 이상형월드컵',
+    cover:'https://mosaic.scdn.co/300/ab67616d00001e02245621ddf8e0d6d27cb14cefab67616d00001e0288605482eff5db0bcb0b8ee5ab67616d00001e02b75c539083b9fc36d7cc7f59ab67616d00001e02bd81b4d2388f936a6f7d815a',
+  }
+]
+
+export function WorldCupListWidget() {
+  return (
+    <Widget>
+      
+    </Widget>
+  )
+}
